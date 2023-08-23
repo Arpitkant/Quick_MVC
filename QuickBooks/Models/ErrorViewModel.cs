bc1720be0651e73c127a06f1075d2a651ef3b159
Arpit kant <kantarpit6@gmail.com>
2023-08-23 12:43:57 +0530

    Add project files.

---
+++ QuickBooks/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuickBooks.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
